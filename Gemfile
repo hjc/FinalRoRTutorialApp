25d5e1d1122ac28a454b7b8c5aa066d1af99e67d
Hayden Chudy <hjc1710@gmail.com>
2011-11-14 20:33:54 -0600

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :test do
   gem 'webrat', '0.7.1'
   gem 'spork', '0.8.4'
   gem 'factory_girl_rails', '1.0'
+  gem 'spork', '0.8.4'
 end
 
 # Use unicorn as the web server
--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module FinalRailsTutorialApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
   end
 end
