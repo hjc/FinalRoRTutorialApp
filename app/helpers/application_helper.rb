6f7b9a8a67deba3455c59f02e384b73ca9d37e8f
Hayden Chudy <hjc1710@gmail.com>
2011-11-18 11:13:45 -0600

    Did end of chapter excercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+    return logo
+  end
+  
   #Deal with titles
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
